7a86485853bf86d907022461f9d49c558ed12d1a
Kiril Nikolov <kirilnikolov@Kirils-MacBook-Pro.local>
2016-03-12 15:56:46 +0200

    test(tests): add e2e tests for content rendering

---
+++ e2e/Tabstrip.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tabstrip from '../src/tabstrip/Tabstrip';
+import TabstripContent from '../src/tabstrip/TabstripContent';
+import { withRoot } from 'e2e-utils';
+
+describe('TabstripContent', withRoot(root => {
+    const tabstrip = () => ReactDOM.render( <Tabstrip />, root[0]);
+    const content = () => ReactDOM.render( <TabstripContent {...props}><div>content</div><div>content2</div></TabstripContent>, root[0]);
+    let props = {
+        selected: 0
+    };
+    it('should render a single element', () => {
+        content();
+        const div = root.find('.k-content');
+        expect(div.length).toEqual(1);
+    });
+
+    it('should render children', () => {
+        content();
+        const div = root.find('.k-content');
+        expect(div.children().length).toEqual(1);
+    });
+
+    it('should render k-state-active class', () => {
+        content();
+        const div = root.find('.k-state-active');
+        expect(div.length).toEqual(1);
+    });
+
+    it('should render aria-expanded attr', () => {
+        content();
+        const div = root.find('[aria-expanded = true]');
+        expect(div.length).toEqual(1);
+    });
+
+    it('should render role attr', () => {
+        content();
+        const div = root.find('[role = tabpanel]');
+        expect(div.length).toEqual(1);
+    });
+
+    it('should not render if not selection is present', () => {
+        props.selected = null;
+        content();
+        const div = root.find('.k-content');
+        expect(div.length).toEqual(0);
+    });
+}));
