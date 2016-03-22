2c8e00b61140ba91d60184bb991b40c8952895ae
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2016-03-22 23:17:41 +0200

    refactor(panelbarcontent): refactor code

--- src/panelbar/PanelBarContent.jsx
+++ src/panelbar/PanelBarContent.jsx
@@ -4,25 +4,27 @@ import classNames from 'classnames';
 import styles from '@telerik/kendo-theme-default/styles/panelbar/main';
 
 const propTypes = {
-    active: React.PropTypes.bool,
     children: React.PropTypes.oneOfType([
         React.PropTypes.element,
         React.PropTypes.arrayOf(React.PropTypes.element)
-    ])
+    ]),
+    expanded: React.PropTypes.bool
 };
 
 export default class PanelBarContent extends React.Component {
     render() {
-        let panelBarItemContentClasses = classNames({
+        const { children, expanded } = this.props;
+
+        const panelBarItemContentClasses = classNames({
             [styles['content']]: true
         });
 
         const inlineStyles = {
-            'display': this.props.active ? 'block' : 'none'
+            'display': expanded ? 'block' : 'none'
         };
 
         return (
-            <div className={panelBarItemContentClasses} style={inlineStyles}>{this.props.children}</div>
+            <div className={panelBarItemContentClasses} style={inlineStyles}>{children}</div>
         );
     }
 }
