dc4c753db06cc94d90435117739f1c7c7b234fcf
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2016-03-30 11:17:59 -0400

    docs(panelbar): add new util functions for finding previous, next, first and last items

--- examples/panelbar/util.jsx
+++ examples/panelbar/util.jsx
@@ -4,7 +4,7 @@ import PanelBarNavigation from './../../src/panelbar/PanelBarNavigation';
 import PanelBarContent from './../../src/panelbar/PanelBarContent';
 
 export function mapDataToComponents(map, parentId = null) {
-    const items = (map[parentId] || []).map(item => {
+    return (map[parentId] || []).map(item => {
         let child;
 
         if (map[item.id]) {
@@ -23,13 +23,12 @@ export function mapDataToComponents(map, parentId = null) {
             id: item.id,
             selected: item.selected,
             title: item.title,
-            disabled: item.disabled
+            disabled: item.disabled,
+            focused: item.focused
         };
 
         return (<PanelBarItem {...childProps} key={item.id} />);
     });
-
-    return items;
 }
 
 export function mapItemsByParentId(items) {
@@ -41,4 +40,47 @@ export function mapItemsByParentId(items) {
 
         return itemsByParentId;
     }, {});
+}
+
+export function findPreviousItem(state, targetItem) {
+    const flattedItems = flatVisibleItems(mapItemsByParentId(state));
+
+    const currentIndex = flattedItems.findIndex(item => item.id === targetItem.id);
+
+    return flattedItems[currentIndex > 0 ? currentIndex - 1 : 0];
+}
+
+export function findNextItem(state, targetItem) {
+    const flattedItems = flatVisibleItems(mapItemsByParentId(state));
+
+    const currentIndex = flattedItems.findIndex(item => item.id === targetItem.id);
+
+    return flattedItems[(currentIndex + 1) < flattedItems.length ? (currentIndex + 1) : currentIndex];
+}
+
+export function findFirstItem(state) {
+    return state[state.findIndex(item => (item.parentId === null || item.parentId === undefined) && !item.disabled)];
+}
+
+export function findLastItem(state) {
+    const flattedItems = flatVisibleItems(mapItemsByParentId(state));
+
+    return flattedItems[flattedItems.length - 1];
+}
+
+export function findCurrentLevelItems(targetItem, newState) {
+    return newState.filter((item) => item.parentId === targetItem.parentId);
+}
+
+export function flatVisibleItems(map, flattedItems = [], parentId = null) {
+    (map[parentId] || []).forEach(item => {
+        if (!item.disabled) {
+            flattedItems.push(item);
+            if (map[item.id] && item.expanded) {
+                flatVisibleItems(map, flattedItems, item.id);
+            }
+        }
+    });
+
+    return flattedItems;
 }
